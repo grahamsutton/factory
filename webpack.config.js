4ae4687fa44fa1f7b19a847124a144ee3af2a611
Graham Sutton <grahamsutton2@gmail.com>
2019-05-31 20:49:40 -0400

    Fixed issue with build not exporting properly because of missing commonjs target library.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ module.exports = {
   mode: 'production',
   entry: './factory.js',
   output: {
-  	filename: 'factory.js'
+    filename: 'factory.js',
+    library: 'factory',
+    libraryTarget: 'commonjs2'
   },
   performance: {
-  	hints: false
+    hints: false
   }
 }
